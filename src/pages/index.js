83b13455fc78c1e2bcf62be7aaaa41eea8e8a788
Leo Bernard <admin@leolabs.org>
2018-07-27 12:40:24 +0200

    :mute: Remove console logs

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,6 @@ class BlogIndex extends React.Component {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allMarkdownRemark.edges')
 
-    console.log(siteTitle)
-
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <Helmet title={siteTitle} />
