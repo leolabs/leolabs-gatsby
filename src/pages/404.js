b4b2fba28de59e8835fba8397223e053f0316748
Leo Bernard <admin@leolabs.org>
2018-07-29 23:34:32 +0200

    :lipstick: Properly style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import Layout from '../components/layout'
+
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <Layout>
+    <Helmet>
+      <title>Page not found - {siteTitle}</title>
+    </Helmet>
+
+    <article>
+      <h1>The page you're looking for here doesn't exist</h1>
+    </article>
+  </Layout>
 )
 
 export default NotFoundPage
